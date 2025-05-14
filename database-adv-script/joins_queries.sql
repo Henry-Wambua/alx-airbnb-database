b93eca2b09147ae423082ce3e7f7706ac5774a6f
Wambua Muema <henry.muemaa@gmail.com>
2025-05-14 21:42:13 +0300

    SQL Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,9 +6,10 @@ ON Bookings.user_id = Users.user_id;
 SELECT property_id, review_id
 FROM Property
 LEFT JOIN Reviews
-ON Reviews.property_id = Property.property_id;
+ON Reviews.property_id = Property.property_id
+ORDER BY property_id [ASC];
 
 SELECT user_id, booking_id
 FROM Users
-FULL JOIN Bookings
+FULL OUTER JOIN Bookings
 ON Bookings.user_id = Users.user_id;
