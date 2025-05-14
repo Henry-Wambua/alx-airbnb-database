6a1451f7cd7853952251a069461cfe2243bee469
Wambua Muema <henry.muemaa@gmail.com>
2025-05-14 21:32:10 +0300

    SQL Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,14 @@
+SELECT booking_id, user_id
+FROM Bookings
+INNER JOIN Users
+ON Bookings.user_id = Users.user_id;
 
+SELECT property_id, review_id
+FROM Property
+LEFT JOIN Reviews
+ON Reviews.property_id = Property.property_id;
+
+SELECT user_id, booking_id
+FROM Users
+FULL JOIN Bookings
+ON Bookings.user_id = Users.user_id;
