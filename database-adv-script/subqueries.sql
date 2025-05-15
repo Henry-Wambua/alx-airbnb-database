eb4aa4e344eb2e4d76df5d60241850b7861b38f1
Wambua Muema <henry.muemaa@gmail.com>
2025-05-15 15:56:35 +0300

    Update subqueries.

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,19 @@
+SELECT property_id, rating
+FROM Reviews
+WHERE rating >(
+  SELECT AVG(rating)
+  FROM Reviews
+) 
+AND (
+  SELECT AVG(rating)
+  FROM Reviews
+) > 4.0;
 
+SELECT user_id, booking_id
+FROM Bookings
+WHERE user_id IN (
+  SELECT user_id
+  FROM Bookings
+  GROUP BY user_id
+  HAVING COUNT(*) > 3
+);
