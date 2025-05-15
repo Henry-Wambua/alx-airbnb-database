d3667fb2edf65c3c81c6453f7f6d9097d7455834
Wambua Muema <henry.muemaa@gmail.com>
2025-05-15 19:40:58 +0300

    Update aggregations_and_window_functions.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,8 +3,8 @@ FROM Bookings
 GROUP BY user_id;
 
 SELECT property_id, booking_id,
-       RANK () OVER(ORDER BY booking_count DESC) AS Ranking
-       ROW_NUMBER () OVER(ORDER BY booking_count DESC) AS Row_count
+       RANK() OVER(ORDER BY booking_count DESC) AS Ranking
+       ROW_NUMBER() OVER(ORDER BY booking_count DESC) AS Row_count
 FROM 
 (SELECT property_id, COUNT (*) AS booking_count
 FROM Bookings
