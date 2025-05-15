19542489742c29dfb3d3628c35e79fc474545ddb
Wambua Muema <henry.muemaa@gmail.com>
2025-05-15 19:39:02 +0300

    Aggregations_and_window_functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,13 @@
+SELECT user_id, COUNT(*)
+FROM Bookings
+GROUP BY user_id;
+
+SELECT property_id, booking_id,
+       RANK () OVER(ORDER BY booking_count DESC) AS Ranking
+FROM 
+(SELECT property_id, COUNT (*) AS booking_count
+FROM Bookings
+GROUP BY property_id);
+
+
 
